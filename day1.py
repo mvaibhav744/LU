f36e7abee67e5ecfbd596a48dd59df3675d783a6
mvaibhav744 <30724161+mvaibhav744@users.noreply.github.com>
2021-02-24 14:00:28 +0530

    day 1 assignments

---
+++ day1.py
@@ -0,0 +1,9 @@
+###### QUESTION 1 ########
+X=input("Enter the first number  ")
+Y=input("Enter the second number  ")
+print("division=", (float(X/Y)) ,"multiplication=", X*Y ,"sub=", X-Y ,"add=", X+Y)
+
+###### Question 2 ##########
+X=input("Enter the first number  ")          ### Commnet it if you want to use above input
+Y=input("Enter the second number  ")         ### Commnet it if you want to use above input
+print(float(pow(X,Y)))
