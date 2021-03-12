5edb2f6d6c2a040e37654e380dcc3a2818b076e3
vaibh <mishravaibhav744@gmail.com>
2021-03-12 23:39:50 +0530

    test

--- day1.py
+++ day1.py
@@ -3,7 +3,7 @@ X= int(10)
 Y=int(100)
 print("division=", (float(X/Y)) ,"multiplication=", X*Y ,"sub=", X-Y ,"add=", X+Y)
 
-###### Question 2 ##########
+
 #X=input("Enter the first number  ")          ### Commnet it if you want to use above input
 #Y=input("Enter the second number  ")         ### Commnet it if you want to use above input
 print(float(pow(X,Y)))
